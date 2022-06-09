1fa288392d40e7850d2446c69a92ba581d169060
jeo19 <terrakkk@outlook.com>
2022-06-09 10:15:44 +0800

    UI:Add the DataGridview control

--- CuteCat/Form1.cs
+++ CuteCat/Form1.cs
@@ -49,45 +49,11 @@ namespace CuteCat
                 if (cus.FullName == fullName)
                 {
                     ShowDetails(cus);
+                    break;
                 }
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            ArrayList arrayList = new ArrayList();
-            arrayList.Add(0);
-            arrayList.Add(1);
-            arrayList.Add(3);
 
-            arrayList.Insert(2, 2);
-            arrayList.Remove(2);// Remove array item with a value is 2.
-            arrayList.RemoveAt(1);// Remove array index is 1.
-            //arrayList[0]; 0
-            //arrayList[1]; 1
-            //arrayList[2]; 2
-            //arratList[3]; 3
-            int sum = 0;
-            for (int i = 0; i < arrayList.Count; i++)
-            {
-                int num = (int)arrayList[i];//Assign the type to force.
-                sum += num;
-            }
-            int[] intArray = new int[5];
-            ArrayList arrayList1 = new ArrayList();
-            List<int> intList = new List<int>();
-            intList.Add(1);
-            intList.Add(2);
-            int sum2 = 0;
-            for (int i = 0; i < intList.Count; i++)
-            {
-                int value = intArray[i];
-                sum2 += value;
-            }
-            foreach (int value in intList)
-            {
-                sum2 += value;//The result is the same as above
-            }
-        }
     }
 }
